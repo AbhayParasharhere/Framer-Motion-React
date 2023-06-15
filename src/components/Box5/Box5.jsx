68b828470e2407069fd5c53591b44966404b9522
Abhay <abhayparasharr@gmail.com>
2023-06-15 00:03:44 -0700

    Complete box 5 animation using useAnimation hook

--- src/components/Box5/Box5.jsx
+++ src/components/Box5/Box5.jsx
@@ -1,9 +1,44 @@
-import { motion } from 'framer-motion'
+import { motion, spring, useAnimation } from 'framer-motion'
 
 const Box5 = () => {
+  const control = useAnimation()
   return (
     <div className="box-container">
-      <motion.div className="box"></motion.div>
+      <button
+        onClick={() =>
+          control.start({
+            x: 1100,
+            transition: {
+              type: 'spring',
+              stiffness: 30,
+              dampness: 80,
+            },
+          })
+        }
+      >
+        Move Right
+      </button>
+      <button
+        onClick={() => control.start({ x: 0, transition: { duration: 2 } })}
+      >
+        Move Left
+      </button>
+      <button
+        onClick={() =>
+          control.start({ borderRadius: '0', transition: { duration: 2 } })
+        }
+      >
+        Square
+      </button>
+      <button
+        onClick={() =>
+          control.start({ borderRadius: '50%', transition: { duration: 2 } })
+        }
+      >
+        Circle
+      </button>
+      <button onClick={() => control.stop()}>Stop</button>
+      <motion.div className="box" animate={control}></motion.div>
     </div>
   )
 }
